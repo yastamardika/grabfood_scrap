5029de3861dfe6bda688a368dfd77bdaac679dad
yastamardika <rattyasta1@gmail.com>
2022-03-11 03:46:19 +0700

    add config and seeder

---
+++ seeder/seeder.rb
@@ -0,0 +1,16 @@
+pages << {
+    page_type: "search",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    url: "https://portal.grab.com/foodweb/v2/search",
+    method: "POST",
+    body: "{
+      'latlng': '-7.720255510397954,110.36118064456628',
+      'keyword': '',
+      'offset': 0,
+      'pageSize': 32,
+      'countryCode': 'ID'
+    }"
+    vars: {
+      category: "Food in Jogja"
+    }
+}
